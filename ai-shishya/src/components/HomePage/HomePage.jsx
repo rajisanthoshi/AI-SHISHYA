3331c4f8cb22c8a0ca58d5129e7c8741e46cf9e4
Raji Santhoshi Thevendriya Ganeshrajan <133124603+rajisanthoshi@users.noreply.github.com>
2023-10-06 16:40:46 +0530

    Changes

--- ai-shishya/src/components/HomePage/HomePage.jsx
+++ ai-shishya/src/components/HomePage/HomePage.jsx
@@ -3,41 +3,47 @@ import './HomePage.css';
 export class HomePage extends React.Component{
     render(){
         return(
-            <div>
-                <modus-navbar
-                    id="blue-theme"
-                    show-apps-menu
-                    show-help
-                    show-main-menu
-                    show-notifications
-                    variant="blue">
-                    <div slot="main">Render your own main menu.</div>
-                </modus-navbar>
-                <div className='fullpage'>
-                    <div className='model-dropdown'>
-                <div>
-                    <h3>Search in</h3>
-                <modus-checkbox label="model1"></modus-checkbox>
-                <modus-checkbox label="model2"></modus-checkbox>
-                <modus-checkbox label="model3"></modus-checkbox>
-                <modus-checkbox label="model4"></modus-checkbox>
-                <modus-checkbox label="model5"></modus-checkbox>
+            <div className='main-page'>
+                <div className='navbar'>
+                   <modus-navbar
+                       id="blue-theme"
+                       show-apps-menu
+                       variant="blue">
+                    </modus-navbar>
                 </div>
-                <div><modus-dropdown toggle-element-id="toggleElement">
-                <modus-button id="toggleElement" slot="dropdownToggle" show-caret="true">Dropdown</modus-button>
-                <modus-list slot="dropdownList">
-                    <modus-list-item size="condensed">Item 1</modus-list-item>
-                    <modus-list-item size="condensed">Item 2</modus-list-item>
-                    <modus-list-item size="condensed">Item 3</modus-list-item>
-                </modus-list>
-                </modus-dropdown></div>               
-                </div>
-                <div className='textbox'>
-                <modus-text-input
-                    placeholder="Ask AI Shishya"
-                    ></modus-text-input>
-                    <modus-button color="tertiary">Ask</modus-button>
-                </div>
+
+                <div className='container'>
+                    <div className='left-panel'>    
+                        <p>Search in</p>
+                        <div className='checkboxes'>
+                            <modus-list-item size="large"><modus-checkbox label="model1"></modus-checkbox></modus-list-item>
+                            <modus-list-item size="large"><modus-checkbox label="model2"></modus-checkbox></modus-list-item>
+                            <modus-list-item size="large"><modus-checkbox label="model3"></modus-checkbox></modus-list-item>
+                            <modus-list-item size="large"><modus-checkbox label="model4"></modus-checkbox></modus-list-item>
+                            <modus-list-item size="large"><modus-checkbox label="model5"></modus-checkbox></modus-list-item>
+                            <modus-list-item size="large"><modus-checkbox label="model6"></modus-checkbox></modus-list-item>
+                            <modus-list-item size="large"><modus-checkbox label="model7"></modus-checkbox></modus-list-item>
+                            <modus-list-item size="large"><modus-checkbox label="model8"></modus-checkbox></modus-list-item>
+                            <modus-list-item size="large"><modus-checkbox label="model9"></modus-checkbox></modus-list-item>
+                        </div>
+                        <modus-select
+                           id="select-demo-5"
+                           label="Models dropdown"
+                           options-display-prop="display">
+                        </modus-select>                      
+                    </div>
+
+                    <div className='middle-panel'>
+                        <div className='text-box'>
+                           <modus-text-input placeholder="Ask AI Shishya"></modus-text-input>
+                           <modus-button color="tertiary">Ask</modus-button>
+                        </div>
+                        <div className='model-container'>
+                           <iframe>
+
+                           </iframe>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
